2c32c43f6c61029c8169f7acaa92580f96841dfa
xlf655 <269162281@qq.com>
2019-08-28 21:44:47 +0800

    微调更新

--- js/myLoc2.js
+++ js/myLoc2.js
@@ -1,6 +1,12 @@
 window.onLoad  = function(){
     var map = new AMap.Map('container',{
       center:[101.761294,36.678148],
+      layers: [
+            // 卫星
+            new AMap.TileLayer.Satellite(),
+            // 路网
+            new AMap.TileLayer.RoadNet()
+        ],
       zoom:15
     });
     var marker = new AMap.Marker({
@@ -8,6 +14,14 @@ window.onLoad  = function(){
     title: '家'
 });
 map.add(marker);
+AMap.plugin(['AMap.ToolBar','AMap.Scale','AMap.OverView'],function(){
+         map.addControl(new AMap.ToolBar());
+         map.addControl(new AMap.Scale());
+         map.addControl(new AMap.OverView({isOpen:true}));
+ })
+
+
+
 
 }
 var url = 'https://webapi.amap.com/maps?v=1.4.15&key=92296edc84cbe3d485551bfa3ede9012&callback=onLoad';
